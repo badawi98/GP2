c82226381b9a48633455712b17e067507190bd84
Badawi Wawi <bwwawi@gmail.com>
2020-12-08 18:57:52 +0200

    delete js

--- JS/order.js
+++
@@ -1,9 +0,0 @@
-function generateID(){
-    var firstDigit = Math.floor((Math.random() * 10));
-    var secondDigit = Math.floor((Math.random() * 10))* 10;
-    var thirdDigit = Math.floor((Math.random() * 10)) * 100;
-    var fourthDigit = Math.floor((Math.random() * 10)) * 1000;
-    
-    var roomID = firstDigit + secondDigit + thirdDigit + fourthDigit;
-    document.getElementById('orderID').value = roomID;
-}
